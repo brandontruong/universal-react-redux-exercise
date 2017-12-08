4b96de8e007e86997d82341e46645710d2732907
Brandon Truong <brandon.truong@pickles.com.au>
2017-12-08 15:30:28 +1100

    added more test

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,6 +1,12 @@
 import * as actions from './index.jsx'
 
 describe('actions tests', () => {
+    it('loadData should create LOAD_DATA action', () => {
+        expect(actions.loadData(0)).toEqual({
+            type: 'LOAD_DATA',
+            data: 0
+        })
+    })
     it('getModels should create GET_MODELS action', () => {
         expect(actions.getModels(10)).toEqual({
             type: 'GET_MODELS',
